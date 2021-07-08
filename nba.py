30508277468970b37b3e6e5c46ca4adef68c53c0
Adaora Onwumelu <adaora.m.onwumelu@gmail.com>
2021-07-08 14:14:24 +0000

    Code processes id from user player

--- nba.py
+++ nba.py
@@ -8,6 +8,7 @@
 
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import commonplayerinfo
+import requests
 
 loop = 1
 player = {}
@@ -18,13 +19,24 @@ while(loop):
         print("There was no player found matching the name you provided")
     else:
         loop = 0
-print(player)
-print(player)
-print(player)
-# user_id = player.get("id")
-# print(user_id)
+print(player[0])
+user_id = player[0].get("id")
+print(user_id)
 
 
 # Basic Request
-player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
-print(player_info)
+custom_headers = {
+    'Host': 'stats.nba.com',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
+
+# Only available after v1.1.0
+# Proxy Support, Custom Headers Support, Timeout Support (in seconds)
+player_info = commonplayerinfo.CommonPlayerInfo(player_id=user_id, headers=custom_headers, timeout=100)
+print(player_info.available_seasons.get_dict())
